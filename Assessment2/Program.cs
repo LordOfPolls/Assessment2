c7a5b5193fcf68a0a177c073b975a4c9c4d0a2b7
LordOfPolls <ddavidallen13@gmail.com>
2018-03-19 18:33:11 +0000

    Search function with "nearest"

--- Assessment2/Program.cs
+++ Assessment2/Program.cs
@@ -10,7 +10,6 @@ namespace Assessment2
     {
         static void TypeWrite(string text)
         {
-            Console.Title = text;
             for (int i = 0; i < text.Length; i++)
             {
                 Console.Write(text[i]);
@@ -31,11 +30,13 @@ namespace Assessment2
             }
         }
 
-        static string[] Load(string file) {
+        static double[] Load(string file)
+        {
             try
             {
                 string[] f = System.IO.File.ReadAllLines(@file);
-                return (f);
+                Double[] x = Array.ConvertAll(f, Double.Parse);
+                return (x);
             }
             catch
             {
@@ -43,22 +44,24 @@ namespace Assessment2
                 Sleep(1000);
                 Environment.Exit(0);
             }
-            return (new string[10] );//DumbyLineToAllowFunction 
-
+            return (new double[10]);//DumbyLineToAllowFunction 
         }
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Console.CursorVisible = false;
+            Console.Title = "Boot...";
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
             string[] files = { "Close_128.txt", "Change_128.txt", "Open_128.txt", "High_128.txt", "Low_128.txt" };
+
             try
             {
-                Console.ForegroundColor = ConsoleColor.Green;
                 TypeWrite("Loading arrays...");
-                string[] Close_128 = Load($"Resources/{files[0]}");
-                string[] Change_128 = Load($"Resources/{files[1]}");
-                string[] Open_128 = Load($"Resources/{files[2]}");
-                string[] High_128 = Load($"Resources/{files[3]}");
-                string[] Low_128 = Load($"Resources/{files[4]}");
+                double[] Close_128 = Load($"Resources/{files[0]}");
+                double[] Change_128 = Load($"Resources/{files[1]}");
+                double[] Open_128 = Load($"Resources/{files[2]}");
+                double[] High_128 = Load($"Resources/{files[3]}");
+                double[] Low_128 = Load($"Resources/{files[4]}");
                 Console.Clear();
             }
             catch
@@ -72,18 +75,29 @@ namespace Assessment2
             TypeWrite("Arrays Loaded");
             Sleep(1000);
             Console.Clear();
-            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.White;
             TypeWrite($"Choose your array:\n1: {files[0]}\n2: {files[1]}\n3: {files[2]}\n4: {files[3]}\n5: {files[4]}");
-            int chosen_file = Convert.ToInt32(Console.ReadLine()) -1;
-            string[] Choice = Load($"Resources/{files[chosen_file]}");
-            Console.Clear();
+            int chosen_file = Convert.ToInt32(Console.ReadLine()) - 1;
+            double[] Choice = Load($"Resources/{files[chosen_file]}");
+            Console.Title = $"Reading from {files[chosen_file]}";
 
-            DisplayArray(Choice, files, chosen_file);
+            Console.Clear();
+            TypeWrite("Choose mode:\n\n1: Display\n2: Search");
+            int mode = Convert.ToInt32(Console.ReadLine());
+            switch (mode)
+            {
+                case 1:
+                    DisplayArray(Choice, files, chosen_file);
+                    break;
+                case 2:
+                    SearchArray(Choice, files, chosen_file);
+                    break;
+            }
 
             Console.ReadKey();
         }
 
-        static void DisplayArray(string[] Array, string[] files, int chosen_file)
+        static void DisplayArray(double[] Array, string[] files, int chosen_file)
         {
             Console.ResetColor();
             TypeWrite($"{files[chosen_file]} Selected");
@@ -91,19 +105,22 @@ namespace Assessment2
             TypeWrite("1: Assending");
             TypeWrite("2: Descending");
             TypeWrite("3: Raw");
+            TypeWrite("4: Quicksort Assending");
+            TypeWrite("4: Quicksort Descending");
+
             int display_mode = Convert.ToInt32(Console.ReadLine());
             Console.Clear();
             switch (display_mode)
             {
                 case 1:
-                    string[] tempAssend = Array.OrderBy(c => c).ToArray();
+                    double[] tempAssend = Array.OrderBy(c => c).ToArray();
                     for (int i = 0; i < tempAssend.Length; i++)
                     {
                         Console.WriteLine(tempAssend[i]);
                     }
                     break;
                 case 2:
-                    string[] tempDescend = Array.OrderByDescending(c => c).ToArray();
+                    double[] tempDescend = Array.OrderByDescending(c => c).ToArray();
                     for (int i = 0; i < tempDescend.Length; i++)
                     {
                         Console.WriteLine(tempDescend[i]);
@@ -122,6 +139,89 @@ namespace Assessment2
                     DisplayArray(Array, files, chosen_file);
                     return;
             }
+        }
+
+        static void SearchArray(double[] Array, string[] files, int chosen_file)
+        {
+            Console.Clear();
+            Console.ResetColor();
+
+            double searchItem = 0;
+            TypeWrite("What would you like to search for? ");
+            try
+            {
+                searchItem = Double.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                TypeWrite("Invalid search, search must be a floating point int");
+                Sleep(4000);
+                SearchArray(Array, files, chosen_file);
+                return;
+            }
+
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            TypeWrite($"Searching {Array.Length} items");
+            Sleep(1000);
+            bool found = false;
+            Console.Clear();
+            double mDistance = 0; //f*ck the compiler
+            int Index = -1;
+
+            for (int i = 0; i < Array.Length; i++)
+            {
+                var cDistance = Math.Abs(searchItem - Array[i]);
+                if (Index == -1 || cDistance < mDistance)
+                {
+                    mDistance = cDistance;
+                    Index = i;
+                    if (mDistance == 0) //Found the f*cker
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        found = true;
+                        TypeWrite("Sucess");
+                        try //avoids overflows and underflows when outputting
+                        {
+                            TypeWrite($"{searchItem} on line {i + 1}\n\n{i}. {Array[i - 1]}\n{i + 1}. {Array[i]}\n{i + 2}. {Array[i + 1]}");
+                        }
+                        catch
+                        {
+                            try
+                            {
+                                TypeWrite($"{searchItem} on line {i + 1}\n\n{i + 1}. {Array[i]}\n{i + 2}. {Array[i + 1]}\n{i + 3}. {Array[i + 2]}");
+                            }
+                            catch
+                            {
+                                TypeWrite($"{searchItem} on line {i + 1}\n\n{i - 1}. {Array[i - 2]}\n{i}. {Array[i - 1]}\n{i + 1}. {Array[i]}");
+
+                            }
+
+                        }
+                        break;
+                    }
+                }
+            }
+
+            if (found == false)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                TypeWrite($"Unable to find {searchItem}");
+                TypeWrite($"Nearest value was {Array[Index]} on line {Index + 1}");
+            }
+            TypeWrite("search again? y or n");
+            string reply = Console.ReadLine();
+            bool restart;
+            if (reply == "y")
+                restart = true;
+            else
+                restart = false;
+            if (restart)
+                SearchArray(Array, files, chosen_file);
+            return;
+
+
         }
 
     }
